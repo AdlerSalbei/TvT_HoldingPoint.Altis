34558308be96a10fa1e82f820b75de74d8611a5f
Salbei <karsus3@web.de>
2017-01-10 08:33:12 +0100

    Add files via upload

---
+++ ger_des.hpp
@@ -0,0 +1,49 @@
+class ger_des: ger_wood {
+	class AllUnits {
+        
+	};
+
+	class Type {
+        //rifleman
+		class Soldier_F {
+
+		};
+
+        //autorifleman
+		class Soldier_AR_F {
+			
+		};
+
+        //light AT
+		class Soldier_LAT_F {
+			
+		};
+
+        //ammo bearer
+		class Soldier_A_F {
+			
+		};
+
+        //medic
+		class Medic_F {
+			uniform = "";
+			vest = "";
+			backpack = "";
+		};
+
+        //squad leader
+        class Soldier_SL_F {
+			vest = "";
+			backpack = "";
+			headgear = "";
+			
+        };
+
+        //team leader
+		class Soldier_TL_F {
+			vest = "";
+			backpack = "";
+			headgear = "";
+		};
+	};
+};
