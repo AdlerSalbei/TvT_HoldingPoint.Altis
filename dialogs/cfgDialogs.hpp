14966551aadec46d5534194f56c15fd792285552
Salbei <karsus3@web.de>
2017-01-10 09:06:13 +0100

    Add files via upload

---
+++ dialogs/cfgDialogs.hpp
@@ -0,0 +1,16 @@
+#include "ui_toolkit.hpp"
+#include "baseDefines.hpp"
+#include "baseRsc.hpp"
+
+#include "confirmHint\dialog.hpp"
+
+class RscTitles {
+    #include "twoLineHint\title.hpp"
+
+    class Default {
+        idd = -1;
+        fadein = 0;
+        fadeout = 0;
+        duration = 0;
+    };
+};
