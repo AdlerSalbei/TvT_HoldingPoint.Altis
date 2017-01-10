aba9bd5a6077f9efe0a58cad7fe8b8e9c1e48307
Salbei <karsus3@web.de>
2017-01-10 08:34:52 +0100

    Add files via upload

---
+++ cfgFunctions.hpp
@@ -0,0 +1,73 @@
+class uo {
+    class choosePlayArea {
+        class bluforWaitDialog {};
+        class chooseCity {};
+        class chooseSpawn {};
+        class opforWaitDialog {};
+    };
+
+    class common {
+        class createSideMarker {};
+        class createSideMarkerRemote {};
+        class deleteSideMarker {};
+        class deleteSideMarkerRemote {};
+        class diagReport {};
+        class emptyContainer {};
+        class findRandomPos {};
+        class getIslandCfgValue {};
+        class getNumberOfTeammates {};
+        class intro {};
+        class isCommander {};
+        class serverLog {};
+        class sideHint {};
+        class teleport {};
+    };
+
+    class endings {
+        class endByBluKilled {};
+        class endByControl {};
+        class endByOpfKilled {};
+        class endByTimeout {};
+        class endMission {};
+    };
+
+    class init {
+        class initLoadoutFaction {};
+        class initPubVars {};
+        class initSystems {postInit = 1;};
+        class sequentialInit {};
+    };
+
+    class missionObjectives {
+        class bluforBuyAction {};
+        class checkCommandVehicle {};
+        class commandVehicleMarker {};
+        class createCommandVehicle {};
+        class opforBuyAction {};
+        class setCommandVehicle {};
+        class spawnDealer {};
+    };
+
+    class missionstart {
+        class createTrigger {};
+        class findOpfSpawnPos {};
+        class findOpfStartPos {};
+        class findBluStartPos {};
+        class playAreaSetup {};
+        class tpSide {};
+    };
+
+    class setup {
+        class briefingDone {postInit = 1;};
+        class moveToMapStart {postInit = 1;};
+        class setCommandVehicleClass {};
+        class setLocationMarkers {};
+        class setMapRespawnPos {};
+        class setMissionParams {};
+        class setMoney {};
+        class setOriginalSide {};
+        class setTime {};
+        class setupCountdown {};
+        class setWeather {};
+    };
+};
