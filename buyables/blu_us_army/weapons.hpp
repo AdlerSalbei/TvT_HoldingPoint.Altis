0d76040ab7fbbcedda51c9762feb065be4d92d31
Salbei <karsus3@web.de>
2017-01-10 09:20:27 +0100

    Add files via upload

---
+++ buyables/blu_us_army/weapons.hpp
@@ -0,0 +1,46 @@
+class Weapons {
+    displayName = "Weapons & Accessories";
+    kindOf = "Weapons";
+
+    class LIB_Colt_M1911 {
+        displayName = "M1911";
+        price = 50;
+        stock = 200;
+    };
+	
+	class LIB_M1A1_Thompson {
+        displayName = "M1A1 Thompson";
+        price = 500;
+        stock = 20;
+    };
+	
+	class LIB_M1_Garand {
+        displayName = "M1 Garand";
+        price = 2000;
+        stock = 2;
+    };
+	
+	class LIB_M1_Carbine {
+        displayName = "M1 Carbine";
+        price = 2000;
+        stock = 2;
+    };
+	
+	class LIB_M1918A2_BAR {
+        displayName = "M1918A2 BAR";
+        price = 2000;
+        stock = 2;
+    };
+	
+    class LIB_M1903A4_Springfield {
+        displayName = "M1903A4 Springfield";
+        price = 2000;
+        stock = 2;
+    };
+	
+	class LIB_M1A1_Bazooka {
+        displayName = "M1A1 Bazooka";
+        price = 2000;
+        stock = 2;
+    };
+};
