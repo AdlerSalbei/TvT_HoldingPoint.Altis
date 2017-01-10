8678a919bb34e89d8aa46dda173ffc2c646a1f95
Salbei <karsus3@web.de>
2017-01-10 09:21:06 +0100

    Add files via upload

---
+++ buyables/blu_ger_army/ammo.hpp
@@ -0,0 +1,84 @@
+class Ammo {
+    displayName = "Ammunition";
+    kindOf = "Items";
+
+    class lib_8Rnd_9x19 {
+        displayName = "9mm 8Rnd";
+        description = "Used in:<br/>P38";
+        price = 10;
+        stock = 500;
+    };
+
+    class LIB_32Rnd_9x19 {
+        displayName = "9mm 32Rnd";
+        description = "Used in:<br/>MP40";
+        price = 50;
+        stock = 300;
+    };
+
+    class lib_5Rnd_792x57 {
+        displayName = "7.92mm 5Rnd";
+        description = "Used in:<br/>K98k";
+        price = 25;
+        stock = 300;
+    };
+
+    class lib_5Rnd_792x57_t {
+        displayName = "7.92mm 5Rnd tracer Rounds";
+        description = "Used in:<br/>K98k";
+        price = 50;
+        stock = 150;
+    };
+
+    class lib_30Rnd_792x33 {
+        displayName = "7.92mm 30Rnd";
+        description = "Used in:<br/>Sturmgewehr 44";
+        price = 200;
+        stock = 70;
+    };
+
+    class lib_50Rnd_792x57 {
+        displayName = "7.92mm 50Rnd";
+        description = "Used in:<br/>MG42";
+        price = 100;
+        stock = 150;
+    };
+
+    class LIB_1Rnd_RPzB {
+        displayName = "30mm Anti-Panzer Rakete";
+        description = "Used in:<br/>Raketenpanzerbüchse";
+        price = 200;
+        stock = 10;
+    };
+
+    class rhs_mag_smaw_SR {
+        displayName = "SMAW Spotting Rounds";
+        description = "Used in:<br/>SMAW";
+        price = 100;
+        stock = 4;
+    };
+
+    class DemoCharge_Remote_Mag {
+        displayName = "M112 Demo Charge";
+        price = 600;
+        stock = 6;
+    };
+
+    class SatchelCharge_Remote_Mag {
+        displayName = "M183 Satchel Charge";
+        price = 1400;
+        stock = 6;
+    };
+
+    class HandGrenade {
+        displayName = "M67 Fragmentation Grenade";
+        price = 600;
+        stock = 20;
+    };
+
+    class SmokeShell {
+        displayName = "M83 Smoke Grenade";
+        price = 200;
+        stock = 20;
+    };
+};
---
+++ buyables/blu_ger_army/vehicles.hpp
@@ -0,0 +1,89 @@
+class Vehicles {
+    permissionLevel = 3;
+
+    displayName = "Vehicles";
+    kindOf = "Vehicles";
+    spawnEmpty = 1;
+	
+	class LIB_Kfz1 {
+        displayName = "Kraftfahrzeug 1";
+        price = 500;
+        stock = 10;
+    };
+	
+	class LIB_Kfz1_MG42 {
+        displayName = "Kraftfahrzeug 1 mit MG42";
+        price = 4500;
+        stock = 5;
+    };
+	
+	class LIB_opelblitz_open_y_camo {
+        condition = "!uo_missionParam_ISWOODLAND";
+        displayName = "Opelblitz";
+        price = 1000;
+        stock = 20;
+    };
+	
+	class LIB_opelblitz_open_g_camo {
+        condition = "uo_missionParam_ISWOODLAND";
+        displayName = "Opelblitz";
+        price = 1000;
+        stock = 20;
+    };
+	
+	class LIB_SdKfz251_FFV {
+        displayName = "SonderKraftfahrzeug 251";
+        price = 2500;
+        stock = 10;
+    };
+	
+	class LIB_SdKfz_7 {
+        displayName = "SonderKraftfahrzeug 7";
+        price = 3000;
+        stock = 5;
+    };
+	
+	class LIB_PzKpfwIV_H {
+        displayName = "PanzerKampfwagen IV Ausführung H";
+        price = 5000;
+        stock = 3;
+    };
+	
+	class LIB_PzKpfwV {
+        displayName = "PanzerKampfwagen V Panther";
+        price = 6000;
+        stock = 1;
+    };
+	
+    class LIB_PzKpfwVI_B {
+        condition = "uo_missionParam_ISWOODLAND";
+        displayName = "PanzerKampfwagen VI Tiger";
+        price = 7500;
+        stock = 1;
+    };
+
+    class LIB_PzKpfwVI_B_camo {
+        condition = "!uo_missionParam_ISWOODLAND";
+        displayName = "PanzerKampfwagen VI Tiger";
+        price = 7500;
+        stock = 1;
+    };
+	
+	class LIB_PzKpfwVI_E {
+        displayName = "PanzerKampfwagen VI Königstiger";
+        price = 10000;
+        stock = 1;
+    };
+	
+	class LIB_StuG_III_G {;
+        displayName = "Sturmgeschütz III Ausführung G";
+        price = 4000;
+        stock = 2;
+    };
+	
+	class LIB_GrWr34 {
+        displayName = "Granatenwerfer 34";
+        price = 7500;
+        stock = 1;
+    };
+};
