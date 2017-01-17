d5c024daac0db73bacd7a82addab2777e9ab0727
Salbei <karsus3@web.de>
2017-01-17 22:56:41 +0100

    Update blu_rus_army.hpp

--- buyables/blu_rus_army/blu_rus_army.hpp
+++ buyables/blu_rus_army/blu_rus_army.hpp
@@ -1,13 +1,11 @@
 class blu_rus_army {
 	#include "weapons.hpp"
     #include "ammo.hpp"
-	/*
 	#include "uniforms.hpp"
 	#include "vests.hpp"
 	#include "backpacks.hpp"
 	#include "helmets.hpp"
 	#include "misc.hpp"
-	*/
 	#include "items.hpp"
 	#include "vehicles.hpp"
 };
