33ae9f16444748ccdc2c3602e88f1474901d9271
Salbei <karsus3@web.de>
2017-01-19 09:28:19 +0100

    Update weapons.hpp

--- buyables/opf_rus_army/weapons.hpp
+++ buyables/opf_rus_army/weapons.hpp
@@ -1,61 +1,71 @@
 class Weapons {
-    displayName = "Weapons & Accessories";
+    displayName = "Weapons";
     kindOf = "Weapons";
 
     class LIB_PPSh41_m {
-		condition = "uo_missionParam_ISWOODLAND";
-        displayName = "PPSh 41";
-        price = 1200;
-        stock = 2;
+	condition = "uo_missionParam_ISWOODLAND";
+        displayName = "PPSh 41 + 2 Magazins";
+	price = 550;
+	stock = 20;
+	code = "(_this select 0) addItemToVest 'lib_35Rnd_762x25'; (_this select 0) addItemToVest 'lib_35Rnd_762x25';";	
     };
 
     class LIB_PPSh41_d {
-		condition = "!uo_missionParam_ISWOODLAND";
-        displayName = "PPSh 41";
-        price = 2000;
-        stock = 2;
+	condition = "!uo_missionParam_ISWOODLAND";
+        displayName = "PPSh 41 + 2 Magazins";
+        price = 550;
+        stock = 20;
+	code = "(_this select 0) addItemToVest 'lib_35Rnd_762x25'; (_this select 0) addItemToVest 'lib_35Rnd_762x25';";
     };
 
     class LIB_M9130 {
-        displayName = "Mosin-Nagant M9130";
-        price = 1200;
-        stock = 6;
+        displayName = "Mosin-Nagant M9130 + 2 Magazins";
+        price = 300;
+        stock = 150;
+	code = "(_this select 0) addItemToVest 'lib_5Rnd_762x54'; (_this select 0) addItemToVest 'lib_5Rnd_762x54';";
     };
 
     class LIB_M38 {
-        displayName = "Mosin Nagant M38";
-        price = 800;
-        stock = 6;
+        displayName = "Mosin Nagant M38 + 2 Magazins";
+        price = 300;
+        stock = 100;
+	code = "(_this select 0) addItemToVest 'lib_5Rnd_762x54'; (_this select 0) addItemToVest 'lib_5Rnd_762x54';";
     };
 
     class LIB_M44 {
-        displayName = "Mosin Nagant M44";
-        price = 2000;
-        stock = 2;
+        displayName = "Mosin Nagant M44 + 2 Magazins";
+        price = 300;
+        stock = 50;
+	   code = "(_this select 0) addItemToVest 'lib_5Rnd_762x54'; (_this select 0) addItemToVest 'lib_5Rnd_762x54';";
     };
 	class LIB_SVT_40 {
-        displayName = "SVT 40";
-        price = 2000;
-        stock = 2;
+        displayName = "SVT 40 + 2 Magazins";
+        price = 700;
+        stock = 25;
+	code = "(_this select 0) addItemToVest 'lib_5Rnd_762x54'; (_this select 0) addItemToVest 'lib_5Rnd_762x54';";
     };
 	class LIB_DP28 {
-        displayName = "DP";
-        price = 2000;
-        stock = 2;
+        displayName = "DP + 1 Magazins";
+        price = 800;
+        stock = 15;
+	code = "(_this select 0) addItemToVest 'lib_47Rnd_762x54';";
     };
 	class LIB_DT {
-        displayName = "DT";
-        price = 2000;
-        stock = 2;
+        displayName = "DT + 1 Magazins";
+        price = 800;
+        stock = 10;
+	code = "(_this select 0) addItemToVest 'lib_63Rnd_762x54';";
     };
 	class LIB_DT_OPTIC {
-        displayName = "DT (optic)";
-        price = 2000;
-        stock = 2;
+        displayName = "DT with optic + 1 Magazins";
+        price = 1000;
+        stock = 5;
+	code = "(_this select 0) addItemToVest 'lib_63Rnd_762x54';";
     };
 	class LIB_M9130PU {
-        displayName = "Mosin-Nagant M9130 PU";
-        price = 2000;
-        stock = 2;
+        displayName = "Mosin-Nagant M9130 PU + 2 Magazins";
+        price = 2500;
+        stock = 1;
+	code = "(_this select 0) addItemToVest 'lib_5Rnd_762x54'; (_this select 0) addItemToVest 'lib_5Rnd_762x54';";
     };
 };
