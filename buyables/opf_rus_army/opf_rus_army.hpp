5fd147b07c781c0f5abd9a689c736f913fdb1a22
Salbei <karsus3@web.de>
2017-01-19 09:49:29 +0100

    Update opf_rus_army.hpp

--- buyables/opf_rus_army/opf_rus_army.hpp
+++ buyables/opf_rus_army/opf_rus_army.hpp
@@ -1,11 +1,13 @@
 class opf_rus_army {
 	#include "weapons.hpp"
 	#include "ammo.hpp"
+	/*
 	#include "uniforms.hpp"
 	#include "vests.hpp"
 	#include "backpacks.hpp"
 	#include "helmets.hpp"
 	#include "misc.hpp"
+	*/
 	#include "items.hpp"
 	#include "vehicles.hpp"
     #include "defenses.hpp"
