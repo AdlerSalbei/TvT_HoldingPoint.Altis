08bd84a4247ecf41a8c9bd6173d77bcb9a818722
Salbei <karsus3@web.de>
2017-01-19 09:42:52 +0100

    Update helmets.hpp

--- buyables/blu_rus_army/helmets.hpp
+++ buyables/blu_rus_army/helmets.hpp
@@ -1,56 +1,44 @@
 class helmets {
+	displayName = "Helmets";
+	kindOf = "Wearables";
 	
 	class H_LIB_SOV_RA_PrivateCap{
-        kindOf = "Wearables";
-
         displayName = "1";
         price = 10;
         stock = 10;
     };	
 	
 	class H_LIB_SOV_RA_OfficerCap{
-        kindOf = "Wearables";
-
         displayName = "2";
         price = 10;
         stock = 10;
     };	
 	
 	class H_LIB_SOV_RA_Helmet{
-        kindOf = "Wearables";
-
         displayName = "3";
         price = 10;
         stock = 10;
     };
 	
 	class H_LIB_SOV_TankHelmet{
-        kindOf = "Wearables";
-
         displayName = "4";
         price = 10;
         stock = 10;
     };
 	
 	class H_LIB_SOV_PilotHelmet{
-        kindOf = "Wearables";
-
         displayName = "5";
         price = 10;
         stock = 10;
     };
 	
 	class H_LIB_SOV_Ushanka{
-        kindOf = "Wearables";
-
         displayName = "6";
         price = 10;
         stock = 10;
     };
 	
 	class H_LIB_SOV_Ushanka2{
-        kindOf = "Wearables";
-
         displayName = "7";
         price = 10;
         stock = 10;
