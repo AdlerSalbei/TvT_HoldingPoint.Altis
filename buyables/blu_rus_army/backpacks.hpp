300905267070ff99c20c7388d81161ebdbea94b8
Salbei <karsus3@web.de>
2017-01-19 09:38:19 +0100

    Update backpacks.hpp

--- buyables/blu_rus_army/backpacks.hpp
+++ buyables/blu_rus_army/backpacks.hpp
@@ -1,80 +1,62 @@
 class backpacks {
+	displayName = "Backpacks";
+	kindOf = "Wearables";
 	
 	class B_LIB_SOV_RA_GasBag{
-        kindOf = "Wearables";
-
         displayName = "1";
         price = 10;
         stock = 10;
     };	
 	
 	class B_LIB_SOV_RA_Paradrop{
-        kindOf = "Wearables";
-
         displayName = "2";
         price = 10;
         stock = 10;
     };	
 	
 	class B_LIB_SOV_RA_MedicalBag{
-        kindOf = "Wearables";
-
         displayName = "3";
         price = 10;
         stock = 10;
     };
 	
 	class B_LIB_SOV_RA_Radio{
-        kindOf = "Wearables";
-
         displayName = "4";
         price = 10;
         stock = 10;
     };
 	
 	class B_LIB_SOV_RA_Rucksack{
-        kindOf = "Wearables";
-
         displayName = "5";
         price = 10;
         stock = 10;
     };
 	
 	class B_LIB_SOV_RA_Rucksack_Green{
-        kindOf = "Wearables";
-
         displayName = "6";
         price = 10;
         stock = 10;
     };
 
 	class B_LIB_SOV_RA_RucksackMines{
-        kindOf = "Wearables";
-
         displayName = "7";
         price = 10;
         stock = 10;
     };
 	
 	class B_LIB_SOV_RA_RucksackMines2{
-        kindOf = "Wearables";
-
         displayName = "8";
         price = 10;
         stock = 10;
     };
 	
 	class B_LIB_SOV_RA_MGAmmoBagt{
-        kindOf = "Wearables";
-
-        displayName = "9";
+            displayName = "9";
         price = 10;
         stock = 10;
     };
 	
 	class B_LIB_SOV_RA_MGAmmoBag_Empty{
-        kindOf = "Wearables";
-
         displayName = "10";
         price = 10;
         stock = 10;
