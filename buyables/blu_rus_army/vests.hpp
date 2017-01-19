eb1be6c9706ca439964928006eb05633c2e22d4a
Salbei <karsus3@web.de>
2017-01-19 09:45:59 +0100

    Update vests.hpp

--- buyables/blu_rus_army/vests.hpp
+++ buyables/blu_rus_army/vests.hpp
@@ -1,120 +1,92 @@
 class vests {
 	
+	kindOf = "Wearables";
+	
 	class V_LIB_SOV_IShBrVestMG {
-        kindOf = "Wearables";
-
         displayName = "1";
         price = 10;
         stock = 10;
     };	
 	
 	class V_LIB_SOV_IShBrVestPPShMag {
-        kindOf = "Wearables";
-
         displayName = "2";
         price = 10;
         stock = 10;
     };	
 	
 	class V_LIB_SOV_IShBrVestPPShDisc {
-        kindOf = "Wearables";
-
         displayName = "3";
         price = 10;
         stock = 10;
     };
 		
 	class V_LIB_SOV_RA_OfficerVest{
-        kindOf = "Wearables";
-
         displayName = "4";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RA_SniperVest{
-        kindOf = "Wearables";
-
         displayName = "5";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RA_Belt{
-        kindOf = "Wearables";
-
         displayName = "6";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RA_TankOfficerSet{
-        kindOf = "Wearables";
-
         displayName = "7";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RA_PPShBelt{
-        kindOf = "Wearables";
-
         displayName = "8";
         price = 10;
         stock = 10;
     };
 		
 	class V_LIB_SOV_RA_MosinBelt{
-        kindOf = "Wearables";
-
         displayName = "9";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RA_SVTBelt{
-        kindOf = "Wearables";
-
         displayName = "10";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RA_MGBelt{
-        kindOf = "Wearables";
-
         displayName = "11";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RAZV_SVTBelt{
-        kindOf = "Wearables";
-
         displayName = "12";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RAZV_OfficerVest{
-        kindOf = "Wearables";
-
         displayName = "14";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RAZV_MGBelt{
-        kindOf = "Wearables";
-
         displayName = "15";
         price = 10;
         stock = 10;
     };
 	
 	class V_LIB_SOV_RAZV_PPShBelt{
-        kindOf = "Wearables";
-
         displayName = "16";
         price = 10;
         stock = 10;
