3bf81047ee422bb715bb4d659a347a43899503ed
Salbei <karsus3@web.de>
2017-01-19 09:49:08 +0100

    Update uniforms.hpp

--- buyables/blu_rus_army/uniforms.hpp
+++ buyables/blu_rus_army/uniforms.hpp
@@ -1,209 +1,154 @@
 class uniforms {
+	displayName = "Uniforms";
+	kindOf = "Wearables";
 	
 	class U_LIB_SOV_Strelok{
-        kindOf = "Wearables";
-
         displayName = "1";
         price = 10;
         stock = 10;
     };	
 	
 	class U_LIB_SOV_Strelok_summer{
-        kindOf = "Wearables";
-
         displayName = "2";
         price = 10;
         stock = 10;
     };	
 	
 	class U_LIB_SOV_Strelokart{
-        kindOf = "Wearables";
-
         displayName = "3";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Tank_private_field{
-        kindOf = "Wearables";
-
         displayName = "4";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Starshina{
-        kindOf = "Wearables";
-
         displayName = "5";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Efreitor{
-        kindOf = "Wearables";
-
         displayName = "6";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Efreitor_summer{
-        kindOf = "Wearables";
-
         displayName = "7";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Sergeant{
-        kindOf = "Wearables";
-
         displayName = "8";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Pilot{
-        kindOf = "Wearables";
-
         displayName = "9";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Leutenant{
-        kindOf = "Wearables";
-
         displayName = "10";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Artleutenant{
-        kindOf = "Wearables";
-
         displayName = "11";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Stleutenant{
-        kindOf = "Wearables";
-
         displayName = "12";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Kapitan{
-        kindOf = "Wearables";
-
         displayName = "14";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Kapitan_summer{
-        kindOf = "Wearables";
-
         displayName = "15";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Razvedchik_am{
-        kindOf = "Wearables";
-
         displayName = "16";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Razvedchik_lis{
-        kindOf = "Wearables";
-
         displayName = "17";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Razvedchik_mix{
-        kindOf = "Wearables";
-
         displayName = "18";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Razvedchik_autumn{
-        kindOf = "Wearables";
-
         displayName = "19";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Sniper{
-        kindOf = "Wearables";
-
         displayName = "20";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Sniper_spring{
-        kindOf = "Wearables";
-
         displayName = "21";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Sniper_autumn{
-        kindOf = "Wearables";
-
         displayName = "22";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Tank_ryadovoi{
-        kindOf = "Wearables";
-
         displayName = "23";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Tank_sergeant{
-        kindOf = "Wearables";
-
         displayName = "24";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Tank_leutenant{
-        kindOf = "Wearables";
-
         displayName = "25";
         price = 10;
         stock = 10;
     };
 	
 	class U_LIB_SOV_Tank_kapitan{
-        kindOf = "Wearables";
-
         displayName = "26";
         price = 10;
         stock = 10;
     };
-};
-
-		
-		
-		
-		
-		
-		
+};	
