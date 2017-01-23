4118bce3075f259faabc594d7b0a102d765629a2
Salbei <karsus3@web.de>
2017-01-23 13:18:37 +0100

    Add files via upload

--- cfgIslands.hpp
+++ cfgIslands.hpp
@@ -12,7 +12,31 @@ class CfgIslands {
 		spawnPosBlu[] = {5709.2,6239.52,0};
 		spawnPosOpf[] = {6156.56,5841.48,0};
 	};
+	
+	class Beketov {
+		displayName = "Beketov";
+		type = "russian";
+		isWoodland = true;
+		spawnPosBlu[] = {-258.376,308.728,0};
+		spawnPosOpf[] = {-249.061,955.673,0};
+	};
 
+	class Chernarus {
+		displayName = "Chernarus";
+		type = "russian";
+		isWoodland = true;
+		spawnPosBlu[] = {15074.6,14748.2,0};
+		spawnPosOpf[] = {15128.1,13945.9,0};
+	};
+	
+	class Chernarus_winter {
+		displayName = "Chernarus Winter";
+		type = "russian";
+		isWoodland = true;
+		spawnPosBlu[] = {15074.6,14748.2,0};
+		spawnPosOpf[] = {15128.1,13945.9,0};
+	};
+	
 	class Colleville {
 		displayName = "Colleville";
 		isWoodland = true;
@@ -20,6 +44,14 @@ class CfgIslands {
 		spawnPosOpf[] = {7756.23,5303.44,0};
 	};
 	
+	class fata {
+		displayName = "Fata";
+		type = "middle eastern";
+		isWoodland = false;
+		spawnPosBlu[] = {10616.3,8942.11,0};
+		spawnPosOpf[] = {10723.9,10105,0};
+	};
+
 	class ivachev {
 		displayName = "Ivachev";
 		isWoodland = true;
@@ -32,6 +64,30 @@ class CfgIslands {
 		isWoodland = true;
 		spawnPosBlu[] = {7396.98,5339.07,0};
 		spawnPosOpf[] = {7756.23,5303.44,0};
+	};
+	
+	class mbg_celle2 {
+		displayName = "Celle";
+		type = "russian";
+		isWoodland = true;
+		spawnPosBlu[] = {13121.7,12175.6,0};
+		spawnPosOpf[] = {12488.2,12689.2,0};
+	};
+	
+	class Sara_dbe1 {
+		displayName = "United Sahrani";
+		type = "mediterranean";
+		isWoodland = true;
+		spawnPosBlu[] = {17997.3,18808.7,0};
+		spawnPosOpf[] = {18177.5,18120.4,0};
+	};
+
+	class Shapur_BAF {
+		displayName = "Shapur";
+		type = "middle eastern";
+		isWoodland = false;
+		spawnPosBlu[] = {2881.15,1992.91,0};
+		spawnPosOpf[] = {2626.44,2236.07,0};
 	};
 	
 	class Stratis {
@@ -48,10 +104,44 @@ class CfgIslands {
 		spawnPosOpf[] = {4315.79,2452.54,0};
 	};
 	
+	class Takistan {
+		displayName = "Takistan";
+		type = "middle eastern";
+		isWoodland = false;
+		spawnPosBlu[] = {13662.5,3886.99,0};
+		spawnPosOpf[] = {13727.4,4748.37,0};
+	};
+	
 	class Tanoa {
 		displayName = "Tanoa";
 		isWoodland = true;
 		spawnPosBlu[] = {1389.42,947.902,0};
 		spawnPosOpf[] = {1150.11,593.379,0};
 	};
+	
+	class utes {
+		displayName = "Utes";
+		type = "russian";
+		isWoodland = true;
+		spawnPosBlu[] = {4377.08,2324.57,0};
+		spawnPosOpf[] = {4315.79,2452.54,0};
+	};
+
+	class vt5 {
+		displayName = "vt5";
+		type = "russian";
+		isWoodland = true;
+		spawnPosBlu[] = {5709.2,6239.52,0};
+		spawnPosOpf[] = {6156.56,5841.48,0};
+	};
+
+	class Zargabad {
+		displayName = "Zargabad";
+		type = "middle eastern";
+		isWoodland = false;
+		spawnPosBlu[] = {7396.98,5339.07,0};
+		spawnPosOpf[] = {7756.23,5303.44,0};
+	};
 };
+
+	
