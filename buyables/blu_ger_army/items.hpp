61af06aef295461c3cb672f015f17f5e224ffb8a
Salbei <karsus3@web.de>
2017-01-23 12:37:35 +0100

    Fixing a mistake

--- buyables/blu_ger_army/items.hpp
+++ buyables/blu_ger_army/items.hpp
@@ -18,6 +18,10 @@ class Items {
         stock = 40;
     };
 
+    class ACE_epinephrine {class Items {
+    displayName = "Items";
+    kindOf = "Items";
+
     class ACE_fieldDressing {
         displayName = "Bandages (x10)";
         description = "10 simple bandages in sterile packaging.";
@@ -42,6 +46,7 @@ class Items {
         stock = 40;
     };
 	
+   	
     class ACE_bloodIV_250 {
     
 	    displayName = "Blood Bag (250ml)";
@@ -82,6 +87,28 @@ class Items {
         displayName = "Entrenching Tool";
 	description = "Tool to build small trenches.";
         price = 50;
+        stock = 40;
+    };
+};
+
+        displayName = "Epinephrine (x5)";
+        description = "5 epipen autoinjectors. Raises heart rate of patient.";
+        amount = 5;
+        price = 100;
+        stock = 40;
+    };
+
+    class ACE_bloodIV_250 {
+        displayName = "Blood Bag (250ml)";
+        description = "250ml AB+ type blood in a blood bag with IV.";
+        price = 100;
+        stock = 40;
+    };
+
+    class ACE_bloodIV_500 {
+        displayName = "Blood Bag (500ml)";
+		description = "500ml AB+ type blood in a blood bag with IV.";
+        price = 200;
         stock = 40;
     };
 };
--- buyables/blu_ger_army/weapons.hpp
+++ buyables/blu_ger_army/weapons.hpp
@@ -2,45 +2,53 @@ class Weapons {
     displayName = "Weapons & Accessories";
     kindOf = "Weapons";
 
-    class LIB_M1A1_Thompson {
-        displayName = "M1A1 Thompson + 2 Magazins";
+    class LIB_MP40 {
+        displayName = "MP40 Mashienenpistole + 2 Magazins";
         price = 550;
         stock = 20;
-		code = "(_this select 0) addItemToVest 'LIB_30Rnd_45ACP'; (_this select 0) addItemToVest 'LIB_30Rnd_45ACP';";
-    };
-			
-	class LIB_M1_Garand {
-        displayName = "M1 Grand + 2 Magazins";
-        price = 300;
-        stock = 300;
-		code = "(_this select 0) addItemToVest 'LIB_8Rnd_762x63'; (_this select 0) addItemToVest 'LIB_8Rnd_762x63';";
+	code = "(_this select 0) addItemToVest 'LIB_32Rnd_9x19'; (_this select 0) addItemToVest 'LIB_32Rnd_9x19';";
     };
 	
-	class LIB_M1_Carbine {
-        displayName = "M1 Carbine + 2 Magazins";
-        price = 700;
-        stock = 25;
-		code = "(_this select 0) addItemToVest 'LIB_15Rnd_762x33'; (_this select 0) addItemToVest 'LIB_15Rnd_762x33';";
-    };	
-	
-	class LIB_M1903A4_Springfield {
-        displayName = "M1903A4 Springfield + 2 Magazins";
-        price = 2500;
-        stock = 1;
-		code = "(_this select 0) addItemToVest 'LIB_5Rnd_762x63'; (_this select 0) addItemToVest 'LIB_5Rnd_762x63';";
+	class LIB_MP44 {
+        displayName = "Sturmgewehr 44 + 2 Magazins";
+        price = 1200;
+        stock = 10;
+	code = "(_this select 0) addItemToVest 'lib_30Rnd_792x33'; (_this select 0) addItemToVest 'lib_30Rnd_792x33';";
     };
 		
-	class LIB_M1918A2_BAR {
-        displayName = "BAR M1918 + 1 Magazins";
-        price = 800;
-        stock = 30;
-		code = "(_this select 0) addItemToVest 'LIB_20Rnd_762x63';";
+	class LIB_K98 {
+        displayName = "K98k + 2 Magazins";
+        price = 300;
+        stock = 300;
+	code = "(_this select 0) addItemToVest 'lib_5Rnd_792x57'; (_this select 0) addItemToVest 'lib_5Rnd_792x57';";
     };
 	
-	class LIB_M1A1_Bazooka {
-        displayName = "M1A1 Bazooka  + 1 Rocket";
+	class LIB_K98ZF39 {
+        displayName = "Kar98k ZF39 + 2 Magazins";
+        price = 2500;
+        stock = 1;
+	code = "(_this select 0) addItemToVest 'lib_5Rnd_792x57'; (_this select 0) addItemToVest 'lib_5Rnd_792x57';";
+    };
+	
+	class LIB_G43 {
+        displayName = "G43 + 2 Magazins";
+        price = 700;
+        stock = 25;
+	code = "(_this select 0) addItemToVest 'lib_5Rnd_792x57'; (_this select 0) addItemToVest 'lib_5Rnd_792x57';";
+    };
+	
+	class LIB_MG42 {
+        displayName = "MG42 + 1 Magazins";
+        price = 800;
+        stock = 30;
+	code = "(_this select 0) addItemToVest 'lib_50Rnd_792x57';";
+    };
+	
+	class LIB_RPzB {
+        displayName = "Raketenpanzerbüchse  + 1 Rocket";
         price = 1200;
         stock = 5;
-		code = "(_this select 0) addItemToBackpack 'LIB_1Rnd_60mm_M6';";
+	code = "(_this select 0) addItemToBackpack 'LIB_1Rnd_RPzB';";
     };
+	
 };
