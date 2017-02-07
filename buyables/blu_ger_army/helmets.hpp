c9382d5a52b78057c46288303bd63c8a31b40a14
Salbei <karsus3@web.de>
2017-02-07 08:15:02 +0100

    Update helmets.hpp

--- buyables/blu_ger_army/helmets.hpp
+++ buyables/blu_ger_army/helmets.hpp
@@ -1,65 +1,112 @@
-class uniforms {
+class helmets {
+	kindOf = "Wearables";
 	
-	class H_LIB_SOV_RA_PrivateCap{
-        kindOf = "Wearables";
-
+	class H_LIB_GER_Helmet {
         displayName = "1";
-        price = 10;
+        price = 0;
         stock = 10;
     };	
 	
-	class H_LIB_SOV_RA_OfficerCap{
-        kindOf = "Wearables";
-
+	class H_LIB_GER_Helmet_net {
         displayName = "2";
-        price = 10;
+        price = 0;
         stock = 10;
     };	
 	
-	class H_LIB_SOV_RA_Helmet{
-        kindOf = "Wearables";
-
+	class H_LIB_GER_Helmet_ns {
         displayName = "3";
-        price = 10;
+        price = 0;
         stock = 10;
     };
 	
-	class H_LIB_SOV_TankHelmet{
-        kindOf = "Wearables";
-
+	class H_LIB_GER_Helmet_painted {
         displayName = "4";
-        price = 10;
+        price = 0;
         stock = 10;
     };
 	
-	class H_LIB_SOV_PilotHelmet{
-        kindOf = "Wearables";
-
+	class H_LIB_GER_Helmet_net_painted {
         displayName = "5";
-        price = 10;
+        price = 0;
         stock = 10;
     };
 	
-	class H_LIB_SOV_Ushanka{
-        kindOf = "Wearables";
-
+	class H_LIB_GER_Helmet_ns_painted {
         displayName = "6";
-        price = 10;
+        price = 0;
         stock = 10;
     };
 	
-	class H_LIB_SOV_Ushanka2{
-        kindOf = "Wearables";
-
+	class H_LIB_GER_HelmetCamo {
         displayName = "7";
-        price = 10;
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_HelmetCamo2 {
+        displayName = "8";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_OfficerCap {
+        displayName = "9";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_Cap {
+        displayName = "10";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_Fieldcap {
+        displayName = "11";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_LW_PilotHelmet {
+        displayName = "12";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_Ushanka {
+        displayName = "13";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_TankOfficerCap {
+        displayName = "14";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_TankOfficerCap2 {
+        displayName = "15";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_TankPrivateCap {
+        displayName = "16";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_TankPrivateCap2 {
+        displayName = "17";
+        price = 0;
+        stock = 10;
+    };
+	
+	class H_LIB_GER_SPGPrivateCap {
+        displayName = "18";
+        price = 0;
         stock = 10;
     };
 };
-
-		
-		
-		
-		
-		
-		
+	
