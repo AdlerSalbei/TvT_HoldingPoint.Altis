7239569be6a596f4add211f6c9c7e3ee90f80ab7
Salbei <karsus3@web.de>
2017-02-13 08:40:58 +0100

    Update cfgParams.hpp

--- cfgParams.hpp
+++ cfgParams.hpp
@@ -5,7 +5,7 @@ class Params
         title = "Weather";
         values[] = {-1,0,25,50,75,100};
         texts[] = {"Random","Clear","Cloudy","Overcast","Rainy","Stormy"};
-        default = 0;
+        default = -1;
         };
 
     class TimeOfDay
@@ -13,7 +13,7 @@ class Params
         title = "Time of Day";
         values[] = {7,8,9,10,11,12,13,14,15,16,17,1000};
         texts[] = {"07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "Random"};
-        default = 12;
+        default = 1000;
     };
 
     class BluforFaction
@@ -78,7 +78,7 @@ class Params
     class SetupTime
     {
         title = "Opfor Setup Time (Minutes)";
-        values[] = {5,60,120,180,240,300,360,420,480,540,600};
+        values[] = {1,60,120,180,240,300,360,420,480,540,600};
         texts[] = {"0","1", "2", "3","4","5","6","8","9","10"};
         default = 60;
     };
@@ -88,7 +88,7 @@ class Params
         title = "Ratio to Control the Point - Blufor:Opfor";
         values[] = {1,1.5,2,2.5,3,3.5,4,4.5,5};
         texts[] = {"1:1","1.5:1","2:1","2.5:1","3:1","3.5:1","4:1","4.5:1","5:1"};
-        default = 3;
+        default = 2;
     };
 
     class MoneyFactor
